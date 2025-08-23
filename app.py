be978e0c963fb9db27ada100630752ef7fe4d07d
Arham Malik <gm0564@myamu.ac.in>
2025-08-23 16:22:55 +0530

    Tab icon secured but still some bugs left

--- app.py
+++ app.py
@@ -20,15 +20,11 @@ csrf = CSRFProtect(app)
 #TAB icon
 from flask import send_from_directory
 import os
-
+app = Flask(__name__, static_folder='static')
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/favicon_io'),
+    return send_from_directory(os.path.join(app.root_path, 'static/favicon_ico'),
                                'favicon-32x32.png', mimetype='image/png')
-
-
-
-
 
 # ----- Load Whisper model and AI Assistant -----
 print("Loading Whisper model...")
